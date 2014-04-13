d2858ddb84898ac1bc76f905f199702359c54535
rentzsch <jwr.git@redshed.net>
2014-04-13 00:46:52 -0500

    Working for GitHub.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,12 +2,15 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-
+		
+		var elements = document.querySelectorAll('.social-count');
+		var elementIdx;
+		var el;
+		
+		for (elementIdx = 0; elementIdx < elements.length; elementIdx++) {
+			el = elements[elementIdx];
+			el.style.display = 'none';
+		}	
 	}
-	}, 10);
+	}, 1);
 });
